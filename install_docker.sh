029b356a34970a4c9cf072558a6c437c5e836555
cooliobr <rafael@regs.com.br>
2023-04-11 19:06:31 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,9 +1,9 @@
    #!/bin/bash
    curl -fsSL https://get.docker.com -o get-docker.sh
    sh get-docker.sh
-   curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d'
+   VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
    DESTINATION=/usr/local/bin/docker-compose
    sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
-   ls -l /usr/local/bin/docker-compose
+   ls -l /usr/local/bin/docker-compose && chmod 755 /usr/local/bin/docker-compose
    /usr/local/bin/docker-compose -v
    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
