450e5156d5099f5ce866047eb7475af88ebadb91
cooliobr <rafael@regs.com.br>
2023-10-20 15:42:44 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,10 @@
    #!/bin/bash
    apt install git curl build-essential wget -y
+   bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+service lightdm stop
+rmmod nouveau
    wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.run
    chmod 755 cuda_12.2.2_535.104.05_linux.run
    ./cuda_12.2.2_535.104.05_linux.run --override --driver --toolkit --silent
